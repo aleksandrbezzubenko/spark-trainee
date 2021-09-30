d2e26cfc06664a715ae6455d7c40d29cc327f74c
Volkov Denis <exeynod@gmail.com>
2021-09-30 23:26:42 +0300

    initial commit

---
+++ src/main/scala/lec1/Aggregations.scala
@@ -0,0 +1,66 @@
+package lec1
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object Aggregations extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Aggregations and Grouping")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+
+  // counting
+  val genresCountDF = moviesDF.select(count(col("Major_Genre"))) // all the values except null
+  moviesDF.selectExpr("count(Major_Genre)")
+
+  // counting all
+  moviesDF.select(count("*")) // count all the rows, and will INCLUDE nulls
+
+  // counting distinct
+  moviesDF.select(countDistinct(col("Major_Genre"))).show()
+
+  // approximate count
+  moviesDF.select(approx_count_distinct(col("Major_Genre")))
+
+  // min and max
+  val minRatingDF = moviesDF.select(min(col("IMDB_Rating")))
+  moviesDF.selectExpr("min(IMDB_Rating)")
+
+  // sum
+  moviesDF.select(sum(col("US_Gross")))
+  moviesDF.selectExpr("sum(US_Gross)")
+
+  // avg
+  moviesDF.select(avg(col("Rotten_Tomatoes_Rating")))
+  moviesDF.selectExpr("avg(Rotten_Tomatoes_Rating)")
+
+  // data science
+  moviesDF.select(
+    mean(col("Rotten_Tomatoes_Rating")),
+    stddev(col("Rotten_Tomatoes_Rating"))
+  )
+
+  // Grouping
+
+  val countByGenreDF = moviesDF
+    .groupBy(col("Major_Genre")) // includes null
+    .count()  // select count(*) from moviesDF group by Major_Genre
+
+  val avgRatingByGenreDF = moviesDF
+    .groupBy(col("Major_Genre"))
+    .avg("IMDB_Rating")
+
+  val aggregationsByGenreDF = moviesDF
+    .groupBy(col("Major_Genre"))
+    .agg(
+      count("*").as("N_Movies"),
+      avg("IMDB_Rating").as("Avg_Rating")
+    )
+    .orderBy(col("Avg_Rating"))
+}
