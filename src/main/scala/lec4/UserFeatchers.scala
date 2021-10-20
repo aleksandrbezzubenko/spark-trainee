c33ae8f52b6aa15457d702d54528be7f6e000bfb
aleksandrbezzubenko <aleksandrbezzubenko.iu8@gmail.com>
2021-10-21 00:48:14 +0300

    bug fix

--- src/main/scala/lec4/UserFeatchers.scala
+++ src/main/scala/lec4/UserFeatchers.scala
@@ -2,6 +2,8 @@ import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, collect_list, lead, sum, udf, when}
 import scala.collection.mutable
+import com.typesafe.config.{Config, ConfigFactory}
+import scala.collection.JavaConverters.asScalaBufferConverter
 
 object UserFeatchers {
 
@@ -9,7 +11,7 @@ object UserFeatchers {
     val spark: SparkSession = SparkSession.builder()
       .appName("DataFrames Exercises")
       .config("spark.master", "local")
-      //.config("spark.driver.bindAddress", "127.0.0.1")
+      .config("spark.driver.bindAddress", "127.0.0.1")
       .getOrCreate()
 
     spark.sparkContext.setLogLevel("WARN")
@@ -17,27 +19,31 @@ object UserFeatchers {
     val eventDtCol = col("event_dt")
     val dataFilter = (eventDtCol > lowerBound) and (eventDtCol < upperBound)
 
-
+    val toSelect: Config = ConfigFactory.load("to_select")
+    val event: Seq[String] = toSelect.getStringList("to_select.event").asScala.toSeq
+    val extFact: Seq[String] = toSelect.getStringList("to_select.ext_fact").asScala.toSeq
+    val resolutions: Seq[String] = toSelect.getStringList("to_select.resolutions").asScala.toSeq
+    
     val eventDtDF = spark.read
       .parquet(s"${path}/exvent")
       .filter(dataFilter)
-      .select("event_id", "transaction_amount")
+      .select(event.head, event.tail:_*)
 
     val extFactDtDF = spark.read
       .parquet(s"${path}/ext_fact")
       .filter(dataFilter)
-      .select("user_id", "cards_payee_card_number", "event_id")
+      .select(extFact.head, extFact.tail:_*)
 
     val resolutionsDtDF = spark.read
       .parquet(s"${path}/resolutions")
       .filter(dataFilter)
-      .select("event_id", "resolution", "created")
+      .select(resolutions.head, resolutions.tail:_*)
 
     val windowSpec = Window.partitionBy("event_id").orderBy("created")
     val lastResolutions = resolutionsDtDF
-      .withColumn("lead",
-        lead("resolution",1).over(windowSpec))
+      .withColumn("lead", lead("resolution",1).over(windowSpec))
       .filter(col("lead").isNull).drop("lead")
+    println("successes")
 
     val gettersCount = udf { arr: mutable.WrappedArray[String] =>
       arr.distinct.map(e => e -> arr.count(_ == e)).toMap
@@ -51,8 +57,7 @@ object UserFeatchers {
       lastResolutions,
       Seq("event_id"),
       "left"
-    ).withColumn("is_fraud", col("resolution")
-      .isin("F","S") or col("resolution").isNull)
+    ).withColumn("is_fraud", col("resolution").isin("F","S") or col("resolution").isNull)
       .groupBy(col("user_id"))
       .agg(
         sum("transaction_amount").as("sum_transaction_amount"),
@@ -61,7 +66,7 @@ object UserFeatchers {
         collect_list("cards_payee_card_number").as("getters")
       ).withColumn("getters_count", gettersCount(col("getters")))
 
-     // writing result
+    // writing result
     resultDF.write
       .mode(SaveMode.Overwrite)
       .save(s"${path}/result")
